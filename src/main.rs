dc7333b49ad4f22c2337e5f09fe6cde21c822c86
Coshiloco <pablohurtadohg@gmail.com>
2023-11-18 13:56:29 +0100

    Avanzando de forma tontorrona

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,13 @@ use serenity::{
     model::{channel::Message, gateway::Ready},
     prelude::*,
     Client,
-    framework::standard::{
-        StandardFramework,
-        CommandResult,
-        macros::{
-            command,
-            group
-        }
-    },
-    http::Http,
+    framework::standard::StandardFramework,
     model::gateway::GatewayIntents,
 };
 use std::process::Command;
 use tempfile::NamedTempFile;
+use dotenv::dotenv;
+use std::env;
 
 struct Handler;
 
@@ -23,45 +17,64 @@ struct Handler;
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content.starts_with("!compile") {
-            let code = &msg.content["!compile".len()..].trim(); // Extraer el código del mensaje y recortar espacios en blanco
+            if let Some(code_block_start) = msg.content.find("```rust") {
+                let code_block_trimmed = &msg.content[code_block_start + 7..];
+                if let Some(code_block_end) = code_block_trimmed.find("```") {
+                    let code = &code_block_trimmed[..code_block_end].trim();
 
-            let mut file = NamedTempFile::new().unwrap();
-            std::fs::write(file.path(), code).unwrap(); // Escribir el código en un archivo temporal
+                    let full_code = format!("fn main() {{\n{}\n}}", code);
+                    let mut file = NamedTempFile::new().expect("Failed to create temp file");
+                    std::fs::write(file.path(), full_code).expect("Failed to write to temp file");
 
-            let crate_name = "user_code"; // Define un nombre de crate válido
-            let output = Command::new("rustc")
-                .arg(file.path())
-                .arg("--out-dir")
-                .arg(file.path().parent().unwrap())
-                .arg("--crate-name")
-                .arg(crate_name) // Asigna el nombre de crate
-                .output()
-                .unwrap();
+                    let output = Command::new("rustc")
+                        .arg(file.path())
+                        .arg("--out-dir")
+                        .arg(file.path().parent().unwrap())
+                        .arg("--crate-name")
+                        .arg("user_code")
+                        .output()
+                        .expect("Failed to execute process");
 
-            // Enviar la salida del compilador al canal
-            if output.status.success() {
-                let executable = file.path().parent().unwrap().join(crate_name);
-                let execution_output = Command::new(executable)
-                    .output()
-                    .unwrap();
+                    if output.status.success() {
+                        let executable = file.path().parent().unwrap().join("user_code");
+                        let execution_output = Command::new(&executable)
+                            .output()
+                            .expect("Failed to execute binary");
 
-                if execution_output.status.success() {
-                    let message = format!("Output:\n```\n{}\n```", 
-                        String::from_utf8_lossy(&execution_output.stdout));
-                    let _ = msg.channel_id.say(&ctx.http, message).await;
+                        if execution_output.status.success() {
+                            let stdout = String::from_utf8_lossy(&execution_output.stdout);
+                            let message = if stdout.is_empty() {
+                                "No output".to_string()
+                            } else {
+                                format!("Output:\n```\n{}\n```", stdout)
+                            };
+                            let _ = msg.channel_id.say(&ctx.http, message).await;
+                        } else {
+                            let stderr = String::from_utf8_lossy(&execution_output.stderr);
+                            let message = format!("Execution Error:\n```\n{}\n```", stderr);
+                            let _ = msg.channel_id.say(&ctx.http, message).await;
+                        }
+                    } else {
+                        let stderr = String::from_utf8_lossy(&output.stderr);
+                        let message = format!("Compilation Error:\n```\n{}\n```", stderr);
+                        let _ = msg.channel_id.say(&ctx.http, message).await;
+                    }
                 } else {
-                    let message = format!("Execution Error:\n```\n{}\n```", 
-                        String::from_utf8_lossy(&execution_output.stderr));
-                    let _ = msg.channel_id.say(&ctx.http, message).await;
+                    let _ = msg.channel_id.say(&ctx.http, "Error: No se encontró el final del bloque de código Rust.").await;
                 }
             } else {
-                let message = format!("Compilation Error:\n```\n{}\n```", 
-                    String::from_utf8_lossy(&output.stderr));
-                let _ = msg.channel_id.say(&ctx.http, message).await;
+                let _ = msg.channel_id.say(&ctx.http, "Error: Por favor, proporciona el código en un bloque de código Rust.").await;
             }
+        } else if msg.content == "!example" {
+            let example_code = r#"```rust
+fn main() {
+    println!("Hello, world!");
+}
+```"#;
+            let _ = msg.channel_id.say(&ctx.http, example_code).await;
         }
     }
-    
+
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
@@ -69,15 +82,14 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
-    let token = "MTE3NTI1MTI1ODI0NDg1Nzg4Ng.GeS9Lm.P8Iw9CEy9QUPAjrMeQFbHXqJmzoP0p96ZB9L3Y"; // Asegúrate de usar un token seguro
+    dotenv().ok();
 
-    // Define los GatewayIntents de tu bot
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
     let intents = GatewayIntents::all();
-
-    // Construye el cliente de Discord con los intents especificados
     let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
-        .framework(StandardFramework::new().configure(|c| c.prefix("!"))) // Configura el prefijo del comando aquí
+        .framework(StandardFramework::new().configure(|c| c.prefix("!")))
         .await
         .expect("Error creating client");
 
